ea4fc4f0e8acafbe4a3e3296841fdaffae5820ff
nonwhen07 <30990876+nonwhen07@users.noreply.github.com>
2024-12-03 15:29:00 +0800

    調整vite更新

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,8 @@ import { fileURLToPath } from 'node:url';
 import path from 'node:path';
 import { glob } from 'glob';
 
-import liveReload from 'vite-plugin-live-reload';
+// 先移除 'vite-plugin-live-reload' 因為尚未支援vite6版本，請vite不一定需要 vite-plugin-live-reload
+//import liveReload from 'vite-plugin-live-reload';
 
 function moveOutputPlugin() {
   return {
@@ -27,13 +28,19 @@ export default defineConfig({
   // base: '/Repository 的名稱/'
   base: '/JS2024_week9/',
   plugins: [
-    liveReload(['./layout/**/*.ejs', './pages/**/*.ejs', './pages/**/*.html']),
+    // 移除了 liveReload 插件
+    //liveReload(['./layout/**/*.ejs', './pages/**/*.ejs', './pages/**/*.html']),
     ViteEjsPlugin(),
     moveOutputPlugin(),
   ],
   server: {
     // 啟動 server 時預設開啟的頁面
     open: 'pages/index.html',
+    // 因移除了 liveReload 插件 而需要 watch 來監聽
+    watch: {
+      // 監聽額外的文件變化（如 .ejs 和 .html）
+      ignored: ['!./layout/**/*.ejs', '!./pages/**/*.ejs', '!./pages/**/*.html'],
+    },
   },
   build: {
     rollupOptions: {
@@ -51,6 +58,5 @@ export default defineConfig({
 
   define: {
     'process.env': process.env
-  }
-  
+  },
 });
